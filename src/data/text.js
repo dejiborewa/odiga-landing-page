9194be0af5d554d5a72f7df578af1ea3eae05472
dejiborewa <ayodejiakinborewa@gmail.com>
2022-01-14 21:08:19 +0100

    updates

---
+++ src/data/text.js
@@ -0,0 +1,21 @@
+import Home from "../assets/home.png";
+import Boy from "../assets/boy.png";
+import Bus from "../assets/bus.png";
+
+export const benefits = [
+    {
+        image: Home,
+        heading: "Welcome to Odigo!",
+        text: "Jump off balcony, onto stranger's head. Chase ball of string hide when guests come over."
+    },
+    {
+        image: Boy,
+        heading: "Your Personal Japan Guide",
+        text: "Jump off balcony, onto stranger's head. Chase ball of string hide when guests come over."
+    },
+    {
+        image: Bus,
+        heading: "Promoting Local Businesses",
+        text: "Jump off balcony, onto stranger's head. Chase ball of string hide when guests come over."
+    }
+];
